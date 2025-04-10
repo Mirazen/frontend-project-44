7272dee667f7c724b87ed9b954c3490ef1728bba
Danya Galenko <danya-galenko@yandex.ru>
2025-04-10 19:06:09 +0300

    step 8

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Mirazen/frontend-project-44/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Mirazen/frontend-project-44/actions)
 [![Maintainability](https://api.codeclimate.com/v1/badges/a2ec5d88c2d70d95cef1/maintainability)](https://codeclimate.com/github/Mirazen/frontend-project-44/maintainability)
-[![asciicast](https://asciinema.org/a/5yFlusMQOL0eRGULaxgrzHhxF.svg)](https://asciinema.org/a/5yFlusMQOL0eRGULaxgrzHhxF)
+[![asciicast](https://asciinema.org/a/JimRos9h7igMdkIIFuapmnYdg.svg)](https://asciinema.org/a/JimRos9h7igMdkIIFuapmnYdg)
---
+++ src/games/progression.js
@@ -0,0 +1,29 @@
+import gameEngine from '../index.js';
+import { getRandomNumber } from '../utils.js';
+
+const generateProgression = (start, step, length) => {
+  const progression = [];
+  for (let i = 0; i < length; i += 1) {
+    progression.push(start + step * i);
+  }
+  return progression;
+};
+
+const generateRound = () => {
+  const progressionLength = getRandomNumber(5, 10);
+  const start = getRandomNumber(1, 20);
+  const step = getRandomNumber(1, 10);
+  const hiddenIndex = getRandomNumber(0, progressionLength - 1);
+
+  const progression = generateProgression(start, step, progressionLength);
+  const correctAnswer = String(progression[hiddenIndex]);
+  progression[hiddenIndex] = '..';
+  const question = progression.join(' ');
+
+  return [question, correctAnswer];
+};
+
+export default () => {
+  const description = 'What number is missing in the progression?';
+  gameEngine(description, generateRound);
+};
